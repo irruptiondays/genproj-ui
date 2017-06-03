e5f5269c20ea0f7113bb185f72b6587d82aeb1ff
TValentine <tev10@my.fsu.edu>
2017-06-03 13:40:16 -0500

    Improved routing.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,16 +3,13 @@
         <nav class="navbar navbar-inverse">
             <div class="container">
                 <div class="navbar-header">
-                    <span class="navbar-brand" id="home-div-link"
-                          @click="goHome">Home</span>
-                    <span class="navbar-brand" id="add-person-div-link"
-                          @click="createPerson">Add Person</span>
-                    <span class="navbar-brand" id="get-all-persons-div-link"
-                          @click="getAllPersons">Get All Persons</span>
-                    <span class="navbar-brand" id="set-family-origin-div-link"
-                          @click="setFamilyOrigin">Set Family Origin</span>
-                    <span class="navbar-brand" id="set-marriages-div-link"
-                          @click="setMarriages">Set Marriages</span>
+                    <ul class="nav nav-pills">
+                        <li role="presentation"><router-link to="/">Home</router-link></li>
+                        <li role="presentation"><router-link to="/add-person">Add Person</router-link></li>
+                        <li role="presentation"><router-link to="/persons-index">Get All Persons</router-link></li>
+                        <li role="presentation"><router-link to="/set-family-origin">Set Family Origin</router-link></li>
+                        <li role="presentation"><router-link to="/set-marriages">Set Marriages</router-link></li>
+                    </ul>
                 </div>
             </div>
         </nav>
@@ -22,21 +19,6 @@
 <script>
     export default {
         methods: {
-            goHome() {
-                this.$router.push('/');
-            },
-            getAllPersons() {
-                this.$router.push('/persons-index');
-            },
-            createPerson() {
-                this.$router.push('/add-person');
-            },
-            setFamilyOrigin() {
-                this.$router.push('/set-family-origin');
-            },
-            setMarriages() {
-                this.$router.push('/set-marriages');
-            }
         }
     }
 </script>
